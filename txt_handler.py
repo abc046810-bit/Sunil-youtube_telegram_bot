69c441c289b0c72c9a510692a6b8db1a933f6bfa
abc046810-bit <abc046810@gmail.com>
2026-02-21 11:20:58 +0530

    Update txt_handler.py

--- txt_handler.py
+++ txt_handler.py
@@ -1,1 +1,15 @@
+from downloader import download_video
 
+def process_txt(file_path):
+    results = []
+
+    with open(file_path, "r") as f:
+        links = f.readlines()
+
+    for link in links:
+        link = link.strip()
+        if link:
+            title, file_path = download_video(link)
+            results.append((title, file_path))
+
+    return results
