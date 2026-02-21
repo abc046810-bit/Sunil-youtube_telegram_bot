19e1991994ce8693e4bd233eb0bdd2b65b6c49cb
abc046810-bit <abc046810@gmail.com>
2026-02-21 11:45:29 +0530

    Delete caption.py

--- caption.py
+++
@@ -1,7 +0,0 @@
-def make_caption(title, vid_id):
-    return (
-        f"🎥 VID_ID : {vid_id}\n\n"
-        f"📌 Title : {title}\n\n"
-        f"📚 Batch Name : UMMEED NEET HINDI 2026\n\n"
-        f"📥 Extracted By : @SunilChoudhary08 ✅"
-    )
