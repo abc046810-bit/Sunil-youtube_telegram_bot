45eb9f7057416df7d7426df13bd4e1dd63dc8885
abc046810-bit <abc046810@gmail.com>
2026-02-21 11:20:32 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,1 +1,19 @@
+import yt_dlp
+import os
 
+def download_video(url):
+    ydl_opts = {
+        "format": "mp4",
+        "outtmpl": "downloads/%(title)s.%(ext)s",
+        "quiet": True
+    }
+
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        title = info.get("title", "video")
+        filename = ydl.prepare_filename(info)
+
+    return title, filename
